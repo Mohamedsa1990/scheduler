0384fde455d65961685c0b0af0371e7ea2a3f0e3
Mohamedsa1990 <Mohameda1990@gamil.com>
2020-10-28 23:36:20 -0600

    adding api data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,15 +1,93 @@
-import React from "react";
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
+import DayList from "components/DayList";
+import Appointment from "components/Appointment";
+
 
 export default function Application(props) {
+  const [day, setDay] = useState("");
+  const [days, setDays] = useState([]);
+
+useEffect(() => {
+  axios.get(`api/days`).then(res => setDays([...res.data]));
+},[])
+
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "3pm",
+  },
+  {
+    id: 4,
+    time: "5pm",
+    interview: {
+      student: "Mohammed",
+      interviewer: {
+        id: 1,
+        name: "Sysad",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "5pm",
+    
+  }
+];
+
+const appointmentsView = appointments.map(appointment => {
+  return (
+    <Appointment
+    key={appointment.id}
+    {...appointment}
+    />
+  );
+})
   return (
     <main className="layout">
       <section className="sidebar">
-        {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+          <DayList
+            days={days}
+            day={day}
+            setDay={setDay}
+          />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointmentsView}
       </section>
     </main>
   );
